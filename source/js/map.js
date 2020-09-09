1f6d658dc1e927c4855a4c855aaa06fc4990a598
iggyfo <iggyfo@gmail.com>
2020-09-10 02:08:40 +0300

    Добавляет карту, добавляет правки по стилезации

---
+++ source/js/map.js
@@ -0,0 +1,27 @@
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 8,
+//     center: { lat: 59.938635, lng: 30.323118 }
+//   });
+
+//   const image ="./img/icon-map-pin.svg";
+//   let marker = new google.maps.Marker({
+//     map,
+//     draggable: true,
+//     animation: google.maps.Animation.DROP,
+//     icon: image,
+//     position: { lat: 59.938635, lng: 30.323118 }
+//   });
+//   marker.addListener("click", toggleBounce);
+// }
+
+// function toggleBounce() {
+//   if (marker.getAnimation() !== null) {
+//     marker.setAnimation(null);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
+
+
+
