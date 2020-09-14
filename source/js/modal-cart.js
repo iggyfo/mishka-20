440aeeb8c97bed938583d8d0796a328387b35661
iggyfo <iggyfo@gmail.com>
2020-09-14 20:48:30 +0300

    Добовляет скрипт на вызов модального окна

--- source/js/modal-cart.js
+++ source/js/modal-cart.js
@@ -1,26 +1,24 @@
 let btnGood = document.querySelector(".button--good");
-let catalogCart = document.querySelector(".catalog-good__link");
 let modalCart = document.querySelector(".modal-cart");
 let modalOverlay = document.querySelector(".modal-overlay");
 let body = document.querySelector("body");
 
-
-
+//Открываю модальное окно и оверлей по клику кнопку заказ
 btnGood.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalCart.classList.add("modal-cart--opened");
-  body.classList.add("modal-overlay__fixed")
-})
+  body.classList.add("modal-overlay__fixed");
+});
 
+//Закрываю модальное окно и оверлей по клику на оверлей
 modalOverlay.addEventListener("click", function(evt) {
-  if (!evt.target === modalCart) {
+  if (evt.target === modalCart) {
     modalCart.classList.remove("modal-cart--opened");
+    body.classList.remove("modal-overlay__fixed")
   }
-}
+});
 
-
-)
-
+//Закрываю модальное окно и оверлей по нажатию ESC
 window.addEventListener("keydown", function(evt) {
   if(evt.keyCode === 27) {
     if(modalCart.classList.contains("modal-cart--opened")) {
@@ -28,11 +26,4 @@ window.addEventListener("keydown", function(evt) {
       modalCart.classList.remove("modal-cart--opened");
     }
   }
-})
-
-// modalOverlay.addEventListener("click", function(evt) {
-//   if(modalCart.classList.contains("modal-cart--opened")) {
-//     evt.preventDefault();
-//     modalCart.classList.remove("modal-cart--opened");
-//   }
-// })
+});
